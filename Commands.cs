09ae582efbcb97d7771c13d887edf1f9a783d030
Alana Gilston <alana@abluescarab.us>
2017-11-25 16:13:28 -0800

    Cleaned up usings

--- Commands.cs
+++ Commands.cs
@@ -1,14 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using Hacknet;
-using ChatSharp;
-using Pathfinder;
-using Pathfinder.Util;
 using System.Text.RegularExpressions;
+using Hacknet;
 
 namespace HacknetIRCLink
 {
--- IRCLink.cs
+++ IRCLink.cs
@@ -1,11 +1,6 @@
-﻿using ChatSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Pathfinder;
+using ChatSharp;
 using Pathfinder.Util;
 
 namespace HacknetIRCLink
